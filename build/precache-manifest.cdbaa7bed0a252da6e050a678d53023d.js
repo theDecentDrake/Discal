f87844d3b8da3f27ca72587c34a368c3d2772786
Faiz-Ahmad-Khan <karbaaz347@gmail.com>
2020-02-06 17:22:59 +0530

    Updated gitignore to exclude build

---
+++ build/precache-manifest.cdbaa7bed0a252da6e050a678d53023d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68509feff1729bb12fe1688db3b9d51a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f71d46d66f12baee46fb",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "a4c5ed230abab254a087",
+    "url": "/static/css/main.97812e71.chunk.css"
+  },
+  {
+    "revision": "f71d46d66f12baee46fb",
+    "url": "/static/js/2.9bc58b61.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9bc58b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c5ed230abab254a087",
+    "url": "/static/js/main.397f5a82.chunk.js"
+  },
+  {
+    "revision": "caa4aa8af9e59589c174",
+    "url": "/static/js/runtime-main.7349f4fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
